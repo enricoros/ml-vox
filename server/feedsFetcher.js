a9727ff96e266292d3a756e9dd095cf9a527552f
Enrico Ros <enrico.ros@gmail.com>
2017-08-16 12:32:10 -0700

    Renamed Accumulator

--- server/feedsFetcher.js
+++ server/feedsFetcher.js
@@ -2,6 +2,8 @@ import {readFileSync, writeFileSync} from 'fs';
 import {FEEDS} from "../src/Feeds";
 import FeedParser, {ellipsize} from "../src/FeedParser";
 
+const OUTPUT_ACCUMULATOR_FILE = 'feed.json';
+
 const readJsonFile = (fileName) => {
   try {
     let data = readFileSync(fileName, 'utf8');
@@ -14,7 +16,7 @@ const readJsonFile = (fileName) => {
 
 const writeJsonFile = (fileName, jsonObject) => writeFileSync(fileName, JSON.stringify(jsonObject/*, null, 2*/), 'utf8');
 
-const Accumulator = readJsonFile('accumulator.json');
+const Accumulator = readJsonFile(OUTPUT_ACCUMULATOR_FILE);
 
 function processNextFeed() {
   const spec = FEEDS.shift();
@@ -51,8 +53,8 @@ function doneWithAllFeeds() {
   posts.sort((a, b) => b.date - a.date);
 
   // out: accumulator
-  console.log('Writing accumulator.json');
-  writeJsonFile('accumulator.json', Accumulator);
+  console.log('Writing ' + OUTPUT_ACCUMULATOR_FILE);
+  writeJsonFile(OUTPUT_ACCUMULATOR_FILE, Accumulator);
 
   // out: posts list
   // console.log('Writing posts.json, with ' + posts.length + ' posts');
